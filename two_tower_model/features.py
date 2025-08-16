30a7d910f777d67f6d232fadf753442798ff866e
Notger Heinz <notger.heinz@gmail.com>
2025-08-16 10:36:19 +0200

    Pre-test user and movie feature generation added

--- two_tower_model/features.py
+++ two_tower_model/features.py
@@ -1,12 +1,29 @@
+"""Functions to generate user and movie features.
+
+IMPORTANT: It is imperative that the user features and the movie features share the same length
+AND are ordered in the same way. Our convention here will be: Features will consist of (N + M)
+entries, where N is then number of movies (one-hot encoded) and M is the number
+of genres.
+
+The genres for the user feature vector will contain the weighted ratings the user gave on movies
+they had watched. For movies, this part will be one-hot encoded as well, obviously.
+
+Please note that this way of doing things assumes that the users have enough ratings to result in
+a meaningful averaged genre score. I.e. their ratings are also sufficiently diverse. A user who only
+watches fantasy action movies but actually only likes the fantasy part will inadvertedly be seen to
+also like the action part and thus be recommended action movies as well.
+"""
+
 import numpy as np
 import pandas as pd
 
 from collections import defaultdict
 
 
-def generate_user_features(aggregated_user_scores: pd.DataFrame, genre_lookup: dict, lookup_genre_to_emb: dict) -> dict:
+def generate_user_features(aggregated_user_scores: pd.DataFrame, genre_lookup: dict, lookup_genre_to_emb: dict) -> pd:DataFrame:
     user_features = {}
-    N_features = max(lookup_genre_to_emb.values()) + 1
+    N_features_genres = max(lookup_genre_to_emb.values()) + 1
+    N_features_movies = max(genre_lookup.keys()) + 1
     
     # Generate the user's genre preferences:
     for row in aggregated_user_scores.itertuples():
@@ -38,11 +55,39 @@ def generate_user_features(aggregated_user_scores: pd.DataFrame, genre_lookup: d
             genre_ratings = {g: (row.avg - 2.5) / 2.5 for g, r in genre_ratings.items()}
     
         # Finally, for each user, we will create a feature vector which contains their genre-ratings.
-        feature = np.zeros(N_features)
+        feature_genres = np.zeros(N_features_genres)
     
         for g, r in genre_ratings.items():
-            feature[g] = r
-    
-        user_features[user_id] = np.array(feature, copy=True)
+            feature_genres[g] = r
 
-    return user_features
+        # Create the one-hot-encoded vector for the movies watched:
+        feature_movies_watched = np.zeros(N_features_movies)
+
+        for movie_voted in movies_voted:
+            feature_movies_watched[movie_voted] = 1.0
+    
+        # Now bring it all together:
+        user_features[user_id] = np.concatenate(
+            (np.array(feature_movies_watched, copy=True), np.array(feature_genres, copy=True),)
+        )
+
+    # We now have a dict of user_id to features, but we have to transform this dict into a DataFrame
+    # where each user comprises a row:
+    return pd.DataFrame.fromt_dict(user_features, orient='index')
+
+
+def generate_movie_features(genre_lookup: dict, lookup_genre_to_emb: dict) -> pd.DataFrame:
+    # This follows the same logic for the user feature function above, except since we are doing
+    # one-hot encoding here and don't need to calculate genre rating averages, we can directly
+    # generate the parts and then glue them together.
+    N_movies = max(genre_lookup.keys()) + 1
+
+    feature_movie_one_hot = np.zeros((N_movies, N_movies))
+    feature_genres = np.zeros((N_movies, max(lookup_genre_to_emb.values()) + 1))
+
+    for k, (movie_id, genres) in enumerate(genre_lookup.items()):
+        feature_movie_one_hot[k, movie_id] = 1.0
+        for genre in genres:
+            feature_genres[k, genre] = 1.0
+
+    return np.concatenate((feature_movie_one_hot, feature_genres), axis=1)
